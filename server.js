cdd646a2de89ee384df7093a22ebfc3a68a61c5b
Wesley L. Handy <wesleylhandy@gmail.com>
2018-01-05 18:01:06 -0500

    add __dirname to static route for build

--- server.js
+++ server.js
@@ -81,9 +81,9 @@ require("./controllers/checkin-controller.js")(app);
 
 // Make public a static dir
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use(express.static(path.join(__dirname, 'client/build')));
     const path = require('path');
-    app.get('/*', function(request, response){
+    app.get('/', function(request, response){
       response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
