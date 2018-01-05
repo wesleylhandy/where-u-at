8fb7e332e54a7efed392d345d9d55c7e7503bfc7
Wesley L. Handy <wesleylhandy@gmail.com>
2018-01-05 18:13:41 -0500

    add path earlier in server

--- server.js
+++ server.js
@@ -81,14 +81,14 @@ require("./controllers/search-controller.js")(app);
 require("./controllers/checkin-controller.js")(app);
 
 // Make public a static dir
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
-    app.use(favicon(path.join(__dirname, 'client', 'build', 'assets', 'favicon.ico')))
-    const path = require('path');
-    app.get('/', function(request, response){
-      response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
+// if (process.env.NODE_ENV === 'production') {
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(favicon(path.join(__dirname, 'assets', 'favicon.ico')))
+app.get('', function(request, response){
+    response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+// }
 
 // Listen on port 3000 or assigned port
 const server = app.listen(app.get('port'), function() {
